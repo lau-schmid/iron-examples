14baa1a9e106dbd254d028c9f659386271e88a26
Benjamin Maier <maierbn@gmail.com>
2016-12-12 19:52:36 +0100

    Adjust bwunicluster job script

--- cuboid/evaluation/parallel_weak_scaling_bwuni/sub_bwunicluster.sh
+++ cuboid/evaluation/parallel_weak_scaling_bwuni/sub_bwunicluster.sh
@@ -3,12 +3,13 @@
 #MSUB -l walltime=12:00:00
 #MSUB -l mem=32gb
 #MSUB -N weak_scaling_on_16x16
-#MSUB -o parallel_weak_scaling_16x16-fwd.txt
+#MSUB -o parallel_weak_scaling_16x16-bwd.txt
 #MSUB -q multinode
 
 export OMP_NUM_THREADS=${MOAB_PROCCOUNT}
 #module load compiler/gnu/4.9
 
-/home/st/st_st/st_smt86072/opencmiss/examples/cuboid/evaluation/parallel_weak_scaling_bwuni/run.sh
-#/home/st/st_st/st_smt86072/opencmiss/examples/cuboid/evaluation/parallel_strong_scaling/run_1.sh
-
+while true; do
+  #/home/st/st_st/st_smt86072/opencmiss/examples/cuboid/evaluation/parallel_weak_scaling_bwuni/run.sh
+  /home/st/st_st/st_smt86072/opencmiss/examples/cuboid/evaluation/parallel_weak_scaling_bwuni/run_backwards.sh
+done

5a190f2c552cc733b5ec3dc88fd2aa4108d361d7
Benjamin Maier <maierbn@gmail.com>
2016-12-22 22:24:23 +0100

    Remove build_release files in ActiveStrain

--- ActiveStrain/build_release/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/local/home/kraemer/software/opencmiss_examples/classicalfield_laplace_simple
-# Build directory: /usr/local/home/kraemer/software/opencmiss_examples/classicalfield_laplace_simple/build_release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(C)
-subdirs(Fortran)

19760a5edfafa1d35db1f8483f5d6ce3daaf62f9
Benjamin Maier <maierbn@gmail.com>
2016-12-22 22:23:12 +0100

    Remove unneeded CMakefiles in ActiveStrain

--- ActiveStrain/build_debug/C/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/local/home/kraemer/software/opencmiss_examples/ActiveStrain/C
-# Build directory: /usr/local/home/kraemer/software/opencmiss_examples/ActiveStrain/build_debug/C
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Laplace_C "laplace_c")
-set_tests_properties(Laplace_C PROPERTIES  ENVIRONMENT "LD_LIBRARY_PATH=/usr/local/home/kraemer/software/opencmiss/iron/install/x86_64_linux/gnu-4.9-F4.9/openmpi_release/debug/bin:")
--- ActiveStrain/build_debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/local/home/kraemer/software/opencmiss_examples/ActiveStrain
-# Build directory: /usr/local/home/kraemer/software/opencmiss_examples/ActiveStrain/build_debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(C)
-subdirs(Fortran)

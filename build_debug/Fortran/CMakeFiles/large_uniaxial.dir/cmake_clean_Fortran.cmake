ec14407ca2347cfd19e93034a736824034f7039c
Wallewam <waleed.mirza890@gmail.com>
2016-12-28 13:43:17 +0100

    my second git commit for documentation

--- build_debug/Fortran/CMakeFiles/large_uniaxial.dir/cmake_clean_Fortran.cmake
+++
@@ -1,6 +0,0 @@
-# Remove fortran modules provided by this target.
-FILE(REMOVE
-  "parsing.mod"
-  "PARSING.mod"
-  "CMakeFiles/large_uniaxial.dir/parsing.mod.stamp"
-  )
--- build_debug/mpi_verification/Fortran/compiler_info.cmake
+++
@@ -1,4 +0,0 @@
-
-                    set(MPI_Fortran_COMPILER_VERSION 4.8.4)
-                    set(MPI_Fortran_COMPILER_ID GNU)
-                    

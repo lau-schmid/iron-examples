fb00fdda392888bf22989ead5b17fde9b0e346a3
Andreas Hessenthaler <hessenthaler@mechbau.uni-stuttgart.de>
2017-01-24 10:14:39 +0100

    Removing crap (temp files, results, etc.)
    - git version control is only for source code, documentation, etc.

--- build_debug/Fortran/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /data/home/mirzawd/software/opencmiss_examples/large_uniaxial_extension/Fortran
-# Build directory: /data/home/mirzawd/software/opencmiss_examples/large_uniaxial_extension/build_debug/Fortran
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Cantilever_Fortran "/data/home/mirzawd/software/opencmiss_examples/large_uniaxial_extension/build_debug/Fortran/large_uniaxial")
-set_tests_properties(Cantilever_Fortran PROPERTIES  ENVIRONMENT "LD_LIBRARY_PATH=/usr/local/opencmiss/iron/debug/install/x86_64_linux/gnu-4.8-F4.8/openmpi_release/debug/bin:")
--- build_debug/mpi_verification/C/compiler_info.cmake
+++
@@ -1,4 +0,0 @@
-
-                    set(MPI_C_COMPILER_VERSION 4.8.4)
-                    set(MPI_C_COMPILER_ID GNU)
-                    

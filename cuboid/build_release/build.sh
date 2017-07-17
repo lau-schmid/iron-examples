2831df6cca2d2964c20dddd2e151accdc9c6797c
Benjamin Maier <benjamin.maier@ipvs.uni-stuttgart.de>
2017-07-17 13:54:07 +0200

    Add ccc alias in build script

--- cuboid/build_release/build.sh
+++ cuboid/build_release/build.sh
@@ -1,1 +1,2 @@
+alias ccc='rm -rf CMakeFiles/ CMakeCache.txt cmake_install.cmake Makefile CTestTestfile.cmake export mpi_verification packaging Tests support'
 ccc && cmake -DOPENCMISS_BUILD_TYPE=RELEASE -DCMAKE_BUILD_TYPE=RELEASE  .. && make clean && make all

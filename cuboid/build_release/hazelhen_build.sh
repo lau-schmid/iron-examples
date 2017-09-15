63ce54a0d3085ec704e572a36033f8b092401771
Benjamin Maier <benjamin.maier@ipvs.uni-stuttgart.de>
2017-09-15 11:18:50 +0200

    update build scripts for cuboid for hazelhen

--- cuboid/build_release/hazelhen_build.sh
+++ cuboid/build_release/hazelhen_build.sh
@@ -7,3 +7,5 @@ export OPENCMISS_INSTALL_DIR=/lustre/cray/ws8/ws/icbbnmai-iron/install
 module restore /lustre/cray/ws8/ws/icbbnmai-iron/manage/build_release/gcc49.module_snapshot
 
 ccc && cmake -DOPENCMISS_BUILD_TYPE=RELEASE -DCMAKE_BUILD_TYPE=RELEASE  .. && make clean && make all
+
+export OPENCMISS_REL_DIR=/zhome/academic/HLRS/cbm/icbbnmai/opencmiss/examples/cuboid/build_release/Fortran
